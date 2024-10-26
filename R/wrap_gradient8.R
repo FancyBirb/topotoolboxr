7c74e0bd2d4a7577fd6c1d5a9cb21ec4fe0aa0c2
FancyBirb <Domenic.ba49@gmail.com>
2024-10-26 18:06:51 +0200

    included topotoolbox.h and made the function gradient8 searchable

---
+++ R/wrap_gradient8.R
@@ -0,0 +1,10 @@
+#' wrap_gradient8
+#'
+#' This will make the Gradient8 funktion available to R from the libtotopotoolbox subdirectory
+#'
+#' @export
+
+wrap_gradient8 <- function() {
+    output <- .c()
+
+}
