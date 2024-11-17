0a39708138c6b7ff11c418e113711b31759b4db2
FancyBirb <Domenic.ba49@gmail.com>
2024-11-17 15:48:26 +0100

    test files and functions to isolate parameter transfer errors

--- R/wrap_gradient8.R
+++ R/wrap_gradient8.R
@@ -4,7 +4,7 @@
 #'
 #' @export
 
-wrap_gradient8 <- function() {
-    output <- .c()
-
+wrap_gradient8 <- function(output,dem,cellsize,use_mp,dims) {
+    result <- .C(C_wrap_gradient8,as.single(output),as.single(dem),as.integer(cellsize),as.integer(use_mp),as.single(dims))
+    return(result[[1]][1])
 }
