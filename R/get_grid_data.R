3d640e46f714fe318b5d911e4b3990d734077cf7
FancyBirb <Domenic.ba49@gmail.com>
2024-11-20 18:43:54 +0100

    changed get_tiff_values.R to get_grid_data.R and added conditions

---
+++ R/get_grid_data.R
@@ -0,0 +1,18 @@
+#' Takes a Spatrast and gives back a matrix with the values of the Spatrast
+#' @param tiff Takes a Spatrast
+
+
+
+get_grid_data <- function(r) {
+    
+    if(terra::nlyr(r) != 1)
+        stop("error: Spatrast has more than one layer");
+    if(terra::xres(r) != terra::yres(r))
+        stop("error: Spatrast's y and x resolutions are not equal");
+    if(terra::is.lonlat(r))
+        stop("error: the coordinate system is not geographic");
+    return(invisible(list(z = terra::values(r,mat=FALSE),
+     cellsize = terra::xres(r),
+     dims = c(terra::ncol(r), terra::nrow(r)))))
+    
+}
