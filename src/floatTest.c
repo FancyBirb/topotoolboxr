5fbff8ccc7d80324f1202d5de28ca09e51fb43b1
FancyBirb <Domenic.ba49@gmail.com>
2024-11-20 23:14:58 +0100

    added test files to isolate the issue with the hand over of ptrdiff_t parameters

--- src/floatTest.c
+++ src/floatTest.c
@@ -1,10 +1,12 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <stdint.h>
 
 void wrap_gradient8(int *output,float *input,ptrdiff_t dims[2],int test[2]){
    
-   printf("%d\n",dims[0]);
-   *output = dims[0];
-   printf("%d",*output);
+   printf("this is the ptrdiff_max: %td\n",PTRDIFF_MAX);
+   printf("%p\n",dims);
+   int *pdims = dims;
+   printf("pdims adress: %p; and pdims content: %td\n",pdims, *pdims);
 
 }
