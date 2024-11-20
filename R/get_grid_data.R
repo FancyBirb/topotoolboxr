b9af25aebf788beae0dfc9be010f1e77d34b3377
FancyBirb <Domenic.ba49@gmail.com>
2024-11-20 18:43:54 +0100

    changed get_tiff_values.R to get_grid_data.R and added conditions

--- R/get_grid_data.R
+++ R/get_grid_data.R
@@ -1,10 +1,6 @@
-#' Takes a Spatrast and gives back a list with the values of the Spatrast, its cellsize and dimensions
-#' @param r Takes a Spatrast
-#' @returns A list with components `z`, a vector 
-#' containing the grid data in row-major order, 
-#' `cellsize`, the horizontal resolution of the grid, 
-#' and `dims` a two-element vector with the number 
-#' of columns and rows of the grid.
+#' Takes a Spatrast and gives back a matrix with the values of the Spatrast
+#' @param tiff Takes a Spatrast
+
 
 
 get_grid_data <- function(r) {
@@ -14,9 +10,9 @@ get_grid_data <- function(r) {
     if(terra::xres(r) != terra::yres(r))
         stop("error: Spatrast's y and x resolutions are not equal");
     if(terra::is.lonlat(r))
-        stop("error: the coordinate system is not projected");
-    return(list(z = terra::values(r,mat=FALSE),
+        stop("error: the coordinate system is not geographic");
+    return(invisible(list(z = terra::values(r,mat=FALSE),
      cellsize = terra::xres(r),
-     dims = c(terra::ncol(r), terra::nrow(r))))
+     dims = c(terra::ncol(r), terra::nrow(r)))))
     
 }
