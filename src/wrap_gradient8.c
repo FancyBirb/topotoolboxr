81feea3acb411e2d4c1184c911953df4c7eba69e
FancyBirb <Domenic.ba49@gmail.com>
2024-11-21 22:05:23 +0100

    added function wrappers for gradient8 and a small help page to use gradient8

--- src/wrap_gradient8.c
+++ src/wrap_gradient8.c
@@ -1,16 +1,29 @@
 #include <stddef.h>
+#include <stdio.h>
 #include <stdint.h>
-
 #include "topotoolbox.h"
 
-#include "topotoolboxr.h"//    This is a working version to run gradient8
-//
+#if defined(_OPENMP)
+#include <omp.h>
+#endif
 
-void wrap_gradient8(float *outputR, float *demR, float *cellsizeR,int *use_mpR, int *dimsR){ //might be usefull to not ask for a ptrdiff_t in the first place
+//    This is a working version to run gradient8
+//
+void wrap_gradient8(float *outputR, float *demR, float *cellsizeR,int *use_mpR, ptrdiff_t *dimsR){ //might be usefull to not ask for a ptrdiff_t in the first place
                                  //and let the user input a long array
+   
+   // printf will be cleared in the future
+   int *pdims = dimsR;
+   printf("pdims adress: %td; and pdims content: %td\n",pdims, *(pdims));
 
    // Uneccessary compicated
-   ptrdiff_t dims [2]= {dimsR[0], dimsR[1]};
+   ptrdiff_t dims [2]= {(long)*(pdims),(long)*(pdims+1)};
+   printf("dims adress: %td; and dims content: %td\n",dims, (dims[0]));
 
-   gradient8(outputR, demR, *cellsizeR, *use_mpR, dims);
+   float cellsize = *cellsizeR;
+   int use_mpC= *use_mpR;
+   float *output = outputR;
+   float *dem = demR;
+   printf("use_mpC: %d\n",use_mpC);
+   gradient8(output,dem,cellsize,use_mpC,dims);
 }
