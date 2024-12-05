2663001fb2b6bda8baa24006a8ec433e5168e808
FancyBirb <Domenic.ba49@gmail.com>
2024-12-05 15:57:35 +0100

    change to header files

--- src/topotoolboxr.h
+++ src/topotoolboxr.h
@@ -3,7 +3,7 @@
 
 void wrap_has_topotoolbox(int *a);
 
-void wrap_gradient8(float *outputR,float *demR, float *cellsizeR,int *use_mpR, int* dimsR);
+void wrap_gradient8(float *outputR,float *demR, float *cellsizeR,int *use_mpR, ptrdiff_t dimsR);
 
 void wrap_fillsink(float *output, float *dem, int *bcR, int *dimsR);
 
