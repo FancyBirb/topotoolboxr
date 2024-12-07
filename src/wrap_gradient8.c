ebb9b5c4dddcfd5d43915098c4762ec2ffbc1e79
FancyBirb <Domenic.ba49@gmail.com>
2024-12-07 16:51:16 +0100

    changed the gradient 8 call to use the variables directly

--- src/wrap_gradient8.c
+++ src/wrap_gradient8.c
@@ -12,9 +12,5 @@ void wrap_gradient8(float *outputR, float *demR, float *cellsizeR,int *use_mpR, 
    // Uneccessary compicated
    ptrdiff_t dims [2]= {dimsR[0], dimsR[1]};
 
-   float cellsize = *cellsizeR;
-   int use_mpC= *use_mpR;
-   float *output = outputR;
-   float *dem = demR;
-   gradient8(output,dem,cellsize,use_mpC,dims);
+   gradient8(outputR, demR, *cellsizeR, *use_mpR, dims);
 }
