5941e8a9419d7e8db2bc561abae889b946f27d31
FancyBirb <Domenic.ba49@gmail.com>
2024-12-11 14:32:55 +0100

    name changes

--- src/wrap_fillsink.c
+++ src/wrap_fillsink.c
@@ -7,6 +7,5 @@ void wrap_fillsink(float *outputR,float *demR, int *dimsR){
    float * output = outputR;
    float * dem = demR;
    ptrdiff_t dims [2]={*dimsR,*(dimsR+1)};
-   printf("dimsR adress %p; dimsR content %td\n",dims,*dims);
-   fillsinks(output,dem,dims);
+   //fillsinks(output,dem,dims);
 }
--- src/wrap_fillsink.h
+++
@@ -1,6 +0,0 @@
-#ifndef WRAP_FILLSINK_H 
-#define WRAP_FILLSINK_H
-
-void wrap_fillsink(float *outputR,float *demR, long *dimsR);
-
-#endif // WRAP_FILLSINK_H
