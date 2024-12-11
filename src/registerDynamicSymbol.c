734bd817ccbe8b96f0c20e8d83dfcf82e2ed054d
FancyBirb <Domenic.ba49@gmail.com>
2024-12-11 13:16:23 +0100

    missnamed gradient8 function

--- src/registerDynamicSymbol.c
+++ src/registerDynamicSymbol.c
@@ -4,20 +4,11 @@
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
 #include "topotoolboxr.h"
-<<<<<<< HEAD
-#include "wrap_gradient8.h"
-#include "wrap_fillsink.h"
-=======
->>>>>>> 1b82f6f (change to header files)
 
 static const R_CMethodDef cMethods[] = {
    {"wrap_has_topotoolbox", (DL_FUNC) &wrap_has_topotoolbox, 1},
-<<<<<<< HEAD
-   {"wrap_gradient8",(DL_FUNC) &wrap_gradient8,5},
    {"wrap_fillsink",(DL_FUNC) &wrap_fillsink,3},
-=======
    {"gradient8",(DL_FUNC) &wrap_gradient8,5},
->>>>>>> 73fe7b9 (rename of wrap_gradient8 and deletion of manual modifications to NAMESPACE)
    {NULL,NULL,0,NULL},
 };
 
